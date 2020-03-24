8a546d9d29ee913bb7ac0b7ce78b74a4cfb0b77c
Pat Shaver <pat.shaver@icloud.com>
2020-03-24 18:27:07 -0400

    Functions

---
+++ script.js
@@ -0,0 +1,19 @@
+// function sayILoveJavaScript() {
+//     console.log("I love Java Script!!")
+// }
+
+// sayILoveJavaScript()
+
+// Write a function that accepts two parameters of name and destination.
+// The function should log a sentence to the console about where that person wants to go.
+// Call the function three different times with different parameters. Example: if you pass in "Jessica" and "Mount Fuji" as parameters, you should see the sentence "Jessica would love to visit Mount Fuji." logged to the console.
+
+function lightening(name, destination) {
+    console.log(`${name} would love to visit ${destination}.`)
+}
+
+lightening("Jessica","Mount Fuji")
+
+lightening("Tommy","Florida")
+
+lightening("Austin","France")
